b3b4390bd004537cf342ef4b2fb8851bd7942bde
Vinícius Viana <viniciusvw18@hotmail.com>
2025-10-23 13:05:22 -0300

    Adicionada alteração da aula 1.09

--- src/main/java/br/com/alura/controller/AgenciaController.java
+++ src/main/java/br/com/alura/controller/AgenciaController.java
@@ -2,6 +2,7 @@ package br.com.alura.controller;
 
 import br.com.alura.domain.Agencia;
 import br.com.alura.service.http.AgenciaService;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.UriInfo;
@@ -10,10 +11,16 @@ import org.jboss.resteasy.reactive.RestResponse;
 @Path("/agencias")
 public class AgenciaController {
 
+
+    /*
     private AgenciaService agenciaService;
     AgenciaController(AgenciaService agenciaService) {
         this.agenciaService = agenciaService;
     }
+
+     */
+    @Inject
+    private AgenciaService agenciaService;
 
     @POST    // informa que o tipo da requisição vai ser POST
     public RestResponse<Void> cadastrar(Agencia agencia, @Context UriInfo uriInfo) {
